49810980e7236fe790ee5bcfd6f34eddb4d6475f
Eduardo <medatech@medasf.org>
2020-07-17 12:48:59 -0700

    If Statements and Conditional Operator == UPDATED

--- assignments/week_5/javascript/scripts/main.js
+++ assignments/week_5/javascript/scripts/main.js
@@ -1,14 +1,16 @@
 // The If Statement
 
 var number1 = 10;
-var number2 = 10;
+var number2 = 15;
 
 // Syntax
+// if (conditional) { group of code to run if conditional is true }
 
 
 
+// Conditional Operators
+// == Compares two values, becomes the boolean true if it is the same, and false if they are not the same.
 
-// Logical Operators
 if (number1 == number2) {
     console.log("This if statement was true so I ran this code!");
 }
