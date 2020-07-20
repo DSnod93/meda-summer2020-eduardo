9f0b176964361c8405b13ab7e002c6141b77ece4
Eduardo <medatech@medasf.org>
2020-07-20 12:43:44 -0700

    JavaScript conditional operators, Review If Statements, Functions

--- assignments/week_6/javascript/scripts/main.js
+++ assignments/week_6/javascript/scripts/main.js
@@ -31,3 +31,74 @@ DEPENDENT OF DATA TYPES
 
 
 */
+
+var data1 = 10;
+var data2 = "10";
+// var data2 = parseInt("10"); // parseFloat() to include periods as decimals.
+
+console.log();
+parseInt();
+
+// Type Coercion
+
+if (data1 === data2) {
+    console.log("The condition was true.");
+} else {
+
+}
+
+
+
+
+// Ternary Operator.
+// condition ? true code : false code;
+
+var isItTrue = false ? "h" : "g";
+
+var isItTrue2;
+if (false) {
+    isItTrue2 = "h";
+} else {
+    isItTrue2 = "g";
+}
+
+console.log(isItTrue);
+// Ternary Operator END
+
+
+
+// Functions
+function sayHelloThreeTimes() {
+    console.log("hello!");
+    console.log("hello!");
+    console.log("hello!");
+}
+
+function sayGoodByeTwoTimes() {
+    console.log("goodbye!");
+    console.log("goodbye!");
+}
+
+function sayHelloNineTimes() {
+    sayHelloThreeTimes();
+    sayHelloThreeTimes();
+    sayHelloThreeTimes();
+}
+
+sayHelloThreeTimes();
+console.log("How are you doing");
+
+
+// function recursiveFunction() {
+//     recursiveFunction();
+// }
+
+sayHelloThreeTimes();
+sayHelloThreeTimes();
+sayGoodByeTwoTimes();
+
+sayHelloNineTimes();
+
+// Functions END
+
+
