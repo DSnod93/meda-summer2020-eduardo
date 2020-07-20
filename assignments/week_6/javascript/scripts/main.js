1ac36d5b8acd3c9e99c538f6faebcf29c0c544ba
Eduardo <medatech@medasf.org>
2020-07-20 11:10:54 -0700

    More JS Operators

---
+++ assignments/week_6/javascript/scripts/main.js
@@ -0,0 +1,33 @@
+// Math Operators;
+// +, -, *, /, %
+// String Operator
+// + (concatenation)
+
+
+/*
+Everything
+in here
+is a comment.
+*/
+
+// Conditional Operators
+// All conditional operations tend to end up being a boolean.
+
+/* 
+
+INDEPENDENT OF DATA TYPES
+== equal to
+<  lesser than
+>  greater than
+!= not equal to
+!  reverses a boolean !true == false    !false == true
+
+<= lesser than or equal
+>= greater than or equal, do not reverse characters as => is arrow function
+
+DEPENDENT OF DATA TYPES
+=== both are equal AND are the same data type
+!==
+
+
+*/
