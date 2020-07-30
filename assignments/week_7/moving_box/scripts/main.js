500347ee4c85c81d0218dc3bfce64160a37e5f4c
Eduardo <medatech@medasf.org>
2020-07-30 12:50:34 -0700

    Refactored Moving Box Code

--- assignments/week_7/moving_box/scripts/main.js
+++ assignments/week_7/moving_box/scripts/main.js
@@ -2,128 +2,58 @@ $(document).ready(function () {
 
     //Buttons Start
     $("#up").click(function () {
-        var currentTopPos = $("#box").css("top");
-        console.log(currentTopPos);
-
-        var topValue = parseInt(currentTopPos);
-        console.log(topValue);
-
-        if (topValue <= 0) {
-            return;
-        }
-
-        var newTopValue = topValue - 50;
-        console.log(newTopValue); 
-
-        var cssTopValue = newTopValue + "px";
-        console.log(cssTopValue);
-
-        $("#box").css("top", cssTopValue);
-
-        // This does not work.
-        //$("#box").css("top", (newTopValue + "px"));
+        moveBox("top", 2, "min", "remove");
     });
 
     $("#left").click(function () {
-        var currentLeftPos = $("#box").css("left");
-
-        var leftValue = parseInt(currentLeftPos);
-
-        if (leftValue <= 0) {
-            return;
-        }
-
-        var newLeftValue = leftValue - 50;
-
-        var cssLeftValue = newLeftValue + "px";
-
-        $("#box").css("left", cssLeftValue);
+        moveBox("left", 2, "min", "remove");
     });
 
     $("#right").click(function () {
-        var currentLeftPos = $("#box").css("left");
-
-        var leftValue = parseInt(currentLeftPos);
-
-        if (leftValue >= 1000) {
-            return;
-        }
-
-        var newLeftValue = leftValue + 50;
-
-        var cssLeftValue = newLeftValue + "px";
-
-        $("#box").css("left", cssLeftValue);
+        moveBox("left", 1000, "max", "add");
     });
 
     $("#down").click(function () {
-        var currentTopPos = $("#box").css("top");
-
-        var topValue = parseInt(currentTopPos);
-
-        if (topValue >= 1000) {
-            return;
-        }
-
-        var newTopValue = topValue + 50;
-
-        var cssTopValue = newTopValue + "px";
-
-        $("#box").css("top", cssTopValue);
+        moveBox("top", 1000, "max", "add");
     });
     //Buttons End
 
     $(document).keydown(function (event) {
-        console.log(event.which);
 
-        // 37 key  == Left Arrow
         if (event.which === 37) {
-            
-
-            var currentLeftPos = $("#box").css("left");
-            var leftValue = parseInt(currentLeftPos);
-            console.log(leftValue);
-            if (leftValue <= 2) {
-                return;
-            }
-            var newLeftValue = leftValue - 50;
-            var cssLeftValue = newLeftValue + "px";
-            $("#box").css("left", cssLeftValue);
-        // 40 key == down Arrow
+            event.preventDefault();
+            moveBox("left", 2, "min", "remove");
         } else if (event.which === 40) {
             event.preventDefault();
-
-            var currentTopPos = $("#box").css("top");
-            var topValue = parseInt(currentTopPos);
-            if (topValue >= 1000) {
-                return;
-            }
-            var newTopValue = topValue + 50;
-            var cssTopValue = newTopValue + "px";
-            $("#box").css("top", cssTopValue);
+            moveBox("top", 1000, "max", "add");
         } else if (event.which === 38) {
             event.preventDefault();
-
-            var currentTopPos = $("#box").css("top");
-            var topValue = parseInt(currentTopPos);
-            if (topValue <= 2) {
-                return;
-            }
-            var newTopValue = topValue - 50;
-            var cssTopValue = newTopValue + "px";
-            $("#box").css("top", cssTopValue);
-
+            moveBox("top", 2, "min", "remove");
         } else if (event.which === 39 ) {
             event.preventDefault();
-
-            var currentLeftPos = $("#box").css("left");
-            var leftValue = parseInt(currentLeftPos);
-            if (leftValue >= 1000) {
-                return;
-            }
-            var newLeftValue = leftValue + 50;
-            var cssLeftValue = newLeftValue + "px";
-            $("#box").css("left", cssLeftValue);
+            moveBox("left", 1000, "max", "add");
         }
     });
 });
+
+function moveBox(cssProperty, limit, minmax, addRemove) {
+    var currentTopPos = $("#box").css(cssProperty);
+
+    var topValue = parseInt(currentTopPos);
+
+    if (minmax === "min") {
+        if (topValue <= limit) {return;}
+    } else if (minmax === "max") {
+        if (topValue >= limit) {return;}
+    }
+
+    if (addRemove === "remove") {
+        var newTopValue = topValue - 50;
+    } else if (addRemove === "add") {
+        var newTopValue = topValue + 50;
+    }
+
+    var cssTopValue = newTopValue + "px";
+
+    $("#box").css(cssProperty, cssTopValue);
+}
